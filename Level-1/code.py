f57eee3dad53ff4620d12cbaee0839282d268808
Kevin Chung <60195141+kev-odin@users.noreply.github.com>
2023-04-26 23:26:55 +0000

    feat: exercise 1 completed!

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,4 +1,4 @@
-'''
+"""
 ////////////////////////////////////////////////////////////
 ///                                                      ///
 ///   0. tests.py is passing but the code is vulnerable  /// 
@@ -9,25 +9,49 @@
 ///   5. Compare your solution with solution.py          ///
 ///                                                      ///
 ////////////////////////////////////////////////////////////
-'''
+"""
+# Based on what I have read on Google and looking at the test cases in the hack.py
+# We are going to need to add constraints to accepted values to prevent an underflow scenario
+# Helpful: https://www.youtube.com/watch?v=sUTulh6qXxY
+
+MAX_AMOUNT = 100_000  # Highest priced item
+MAX_QUANTITY = 10_000  # Limit quantity
+MAX_TOTAL = 1e6  # Absolute limit
 
 from collections import namedtuple
+from decimal import Decimal
 
-Order = namedtuple('Order', 'id, items')
-Item = namedtuple('Item', 'type, description, amount, quantity')
+Order = namedtuple("Order", "id, items")
+Item = namedtuple("Item", "type, description, amount, quantity")
+
 
 def validorder(order: Order):
-    net = 0
-    
+    net = Decimal("0")
+
     for item in order.items:
-        if item.type == 'payment':
-            net += item.amount
-        elif item.type == 'product':
-            net -= item.amount * item.quantity
+        if item.type == "payment":
+            if -1 * MAX_AMOUNT < item.amount < MAX_AMOUNT:
+                net += Decimal(str(item.amount))
+        elif item.type == "product":
+            if 0 < item.quantity <= MAX_QUANTITY and 0 < item.amount <= MAX_AMOUNT:
+                net -= Decimal(str(item.amount)) * item.quantity
+            if not -1 * MAX_TOTAL < net < MAX_TOTAL:
+                return "Your order was not completed due to an internal system error."
         else:
-            return("Invalid item type: %s" % item.type)
-    
+            return f"Invalid item type: {item.type}"
+
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
-    else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return f"Order ID: {order.id} - Payment imbalance: ${net:.2f}"
+    return f"Order ID: {order.id} - Full payment received!"
+
+
+def exploit(n: int = 1050):
+    """Memory overflow at 1022 iterations"""
+    under = 1
+    over = 1
+    factor = 2
+
+    for i in range(n):
+        under = under / factor
+        over = over * factor
+        print(f"Current value: {i} | {under:.2g} | {over:.2g}")
